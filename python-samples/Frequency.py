ad7a8edd1d86c109471cd4c0c9d40f1b0ae905b8
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-12-19 17:37:50 +0530

    minor changes

--- python-samples/Frequency.py
+++ python-samples/Frequency.py
@@ -2,19 +2,23 @@ __author__ = 'ssinghal'
 
 import nltk
 import operator
+from nltk.corpus import stopwords
+
+
 # compute frequence distribution of a data set
 
 file = open("data.txt")
-sentence = file.read()
-words = sentence.split()
-freq = nltk.FreqDist(words)
+sentence = file.read().lower()
+
+word_list = sentence.split()
+
+#remove stop words
+filtered_words = [word for word in word_list if word not in stopwords.words('english')]
+
+freq = nltk.FreqDist(filtered_words)
 
 sorted_freq = sorted(freq.items(), key=operator.itemgetter(1))
 
 for (k, v) in sorted_freq:
     print k , ":", v
-
-
-#for key in sorted_freq.keys():
-#    print key , ":", sorted_freq[key]
 
